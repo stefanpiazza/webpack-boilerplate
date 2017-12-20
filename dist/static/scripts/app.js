1bd2f7da3b290377b9769dcaae199883566eb640
Stefan Piazza <stefanpiazza@gmail.com>
2017-12-20 13:27:21 +0000

    UPDATE: Syntax rewrite with auto HTML generator added

---
+++ dist/static/scripts/app.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var scss = __webpack_require__(0);
+
+/***/ })
+],[1]);
+//# sourceMappingURL=app.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,99 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	context: path.resolve(__dirname, './src'),
-	entry: {
-		main: './js/main.js',
-		// vendor: []
-	},
-	output: {
-		filename: './js/[name].bundle.js',
-		path: path.resolve(__dirname, './dist/'),
-		publicPath: '/',
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, './')
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.(svg|jpg)$/,
-				use: 'url-loader'
-			},
-			{
-				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract({
-	          		fallbackLoader: 'style-loader',
-	          		loader: 'css-loader?importLoaders=1!postcss-loader!sass-loader'
-		        })
-			}
-		]
-	},
-	plugins: [
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'commons',
-			filename: './js/commons.js',
-			minChunks: 2,
-		}),		
-	    new ExtractTextPlugin({
-	    	filename: 'css/[name].bundle.css',
-	    }),
-	    new BrowserSyncPlugin({
+    entry: {
+        app: "./src/js/main.js",
+        // commons: []
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist/"),
+        publicPath: "/",
+        filename: "./static/scripts/[name].js",
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+    },
+    module: {
+        rules: [{
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [{
+                        loader: "file-loader",
+                        options: {
+                            name: "/static/images/[name].[ext]"
+                        }
+                    },
+                    {
+                        loader: "image-webpack-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                            loader: "css-loader",
+                            options: {
+                                camelCase: true,
+                                // Keep same as class definition for now
+                                localIdentName: "[local]",
+                                importLoaders: 2,
+                                modules: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "commons",
+            filename: "static/scripts/commons.js",
+            minChunks: Infinity,
+        }),
+        new ExtractTextPlugin({
+            allChunks: true,
+            filename: "static/styles/[name].css"
+        }),
+        new BrowserSyncPlugin({
 	        host: 'localhost',
 	        port: 3000,
 	        server: {
-	        	baseDir: './'
+	        	baseDir: './dist'
 	        }
+        }),
+        new HtmlWebpackPlugin({
+
         })
-  	]
+    ]
 }
